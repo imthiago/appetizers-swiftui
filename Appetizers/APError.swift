959b25d7877b40539e759f0a875619ec7eb6e0b0
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 23:22:35 -0300

    Adding network manager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Thiago Oliveira on 10/08/22.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
