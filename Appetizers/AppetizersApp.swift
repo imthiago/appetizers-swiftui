08e5907c2d7066c3d0ebfacd1b5ac94e7f56f8b2
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 21:46:26 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Thiago Oliveira on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
