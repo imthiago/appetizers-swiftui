cd8ec7fa629179469f7f896a352281a95eedc8e8
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 21:58:07 -0300

    Initial tab bar setup

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,23 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Thiago Oliveira on 10/08/22.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Appetizer List View")
+                .navigationTitle("🍟 Appetizers")
+        }
+    }
+}
+
+struct AppetizerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListView()
+    }
+}
