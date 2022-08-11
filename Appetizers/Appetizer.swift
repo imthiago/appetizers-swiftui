462e748345a7bf20cf1d772114a7406730ec695d
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 22:31:02 -0300

    Appetizer model

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Thiago Oliveira on 10/08/22.
+//
+
+import Foundation
+
+struct Appetizer: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 1,
+                                           name: "Asian Flank Steak",
+                                           description: "This perfectly thin cut just melts in your mouth.",
+                                           price: 8.99,
+                                           imageURL: "https://seanallen-course-backend.herokuapp.com/images/appetizers/asian-flank-steak.jpg",
+                                           calories: 300,
+                                           protein: 14,
+                                           carbs: 0)
+
+    static let apperizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
