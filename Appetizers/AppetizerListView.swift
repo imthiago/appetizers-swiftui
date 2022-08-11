2691064e2f2d8bda9fd6d4cbfb39786f5f8d0cd7
Thiago Oliveira <thiago@outlook.com.br>
2022-08-10 23:36:27 -0300

    Addind first call with network manager

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+
+    @State private var appetizers: [Appetizer] = []
+
     var body: some View {
         NavigationView {
-            List(MockData.apperizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍟 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
