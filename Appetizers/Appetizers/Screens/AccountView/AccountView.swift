0a86e7b646a60510f6266cb4a7acfa8e1bfa570e
Thiago Oliveira <thiago@outlook.com.br>
2022-10-10 16:34:45 -0300

    add account section

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("😅 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                    DatePicker("BIrthday", selection: $birthday, displayedComponents: .date)
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+                .tint(.brandPrimary)
+            }
+            .navigationTitle("😅 Account")
         }
     }
 }
